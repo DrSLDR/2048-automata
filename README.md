405f0d89b8bc8aa6c56f97cb7611c7519022c491
Jonas A. Hultén <jon.hulten@gmail.com>
2014-11-09 01:13:40 +0100

    Random Automata implemented

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ A fork of [gabrielecirulli's 2048](https://github.com/gabrielecirulli/2048),
 So far, the Chrome app implementation works as intended. Automation code is in
   progress.
 
+So far, the following Automata are implemented:
+* **Random Direction** 
+  Randomly moves the tiles around until it hits game over or the 2048 tile. 
+  Implemented as a proof-of-concept only.
+
+More advanced Automata will be implemented later. 
+
+To use, select an Automata from the dropdown menu and hit spacebar. You can
+  monitor what the Automata is doing using Chrome's debug console.
+
 ## License
 2048 is licensed under the [MIT
   license.](https://github.com/gabrielecirulli/2048/blob/master/LICENSE.txt)
--- js/automata.js
+++ js/automata.js
@@ -2,18 +2,19 @@ function Automata(GameManager) {
     this.gm = GameManager;
 
     this.active = false;
-    this.moveDelay = 1000;
+    this.delay = 500;
 }
 
 // Toggles the Automata
 Automata.prototype.toggle = function () {
     this.active = !this.active;
     if(this.active){
+        targetAutomata = document.getElementsByClassName("automata-select")[0].
+            value;
         console.debug("Automata toggled on");
-        console.debug("Automata in use: " + 
-                      document.getElementsByClassName("automata-select")[0].
-                      value);
+        console.debug("Automata in use: " + targetAutomata);
         setColor = "#FF0000";
+        this.init(targetAutomata);
     }else{
         console.debug("Automata toggled off");
         setColor = "#776E65";
@@ -21,6 +22,15 @@ Automata.prototype.toggle = function () {
     elems = document.getElementsByClassName("title");
     for (i = 0; i < elems.length; i++) {
         elems[i].style.color = setColor;
+    }
+}
+
+// Initiates a specific Automata
+Automata.prototype.init = function (name) {
+    switch(name){
+        case "random":
+        this.randomAutomata();
+        break;
     }
 }
 
@@ -41,4 +51,29 @@ Automata.prototype.sendMove = function (direction) {
         break;
     }
     this.gm.move(direction);
+}
+
+// this-binding workaround (courtesy of MDN)
+Automata.prototype.setTimeoutWO = function (vCallback, nDelay) {
+    if(this.active){
+        var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+        return window.setTimeout(vCallback instanceof Function ? function () {
+            vCallback.apply(oThis, aArgs);
+        } : vCallback, nDelay);
+    }
+};
+
+Automata.prototype.goAhead = function () {
+    return (!this.gm.over && !this.gm.won);
+}
+
+// Random Direction Automata - debug only, really
+Automata.prototype.randomAutomata = function () {
+    if(this.goAhead()){
+        dir = Math.floor(Math.random() * 4);
+        this.sendMove(dir);
+        this.setTimeoutWO(this.randomAutomata,this.delay);
+    }else{
+        this.toggle();
+    }
 }
